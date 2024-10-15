5fb51fa7b7e803a823c051244d4f600fc02eefb5
SR0204 <k023g0023@g.neec.ac.jp>
2024-10-15 10:46:13 +0900

    課題途中

--- main.cpp
+++ main.cpp
@@ -6,15 +6,17 @@ int Recursive(int n) {
 	if (n <= 1) {
 		return (1);
 	}
-	return(n * Recursive(n - 50));
+	return(n * Recursive(100*2-50));
 }
 
 int main() {
-	int n = 2;//階乗する値
+
+	int n = 100;//階乗する値
 	int result;//計算結果
 
 	result = Recursive(n);
-	printf("%dの階乗=%d\n", n, result);
+
+	printf("%d\n", n, result);
 
 	return 0;
 }
