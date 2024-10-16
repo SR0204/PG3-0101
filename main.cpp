1334bb7bcfae3902c9b3d92590ad0fdf1cf19124
SR0204 <k023g0023@g.neec.ac.jp>
2024-10-16 18:29:08 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -4,19 +4,30 @@
 
 int Recursive(int n) {
 	if (n <= 1) {
-		return (1);
+		return (100);
 	}
-	return(n * Recursive(100*2-50));
+	return(Recursive(n-1)*2-50);
 }
 
 int main() {
 
-	int n = 100;//階乗する値
+	int x = 10;//階乗する値
+
+	int y = 1072;//賃金
+
 	int result;//計算結果
 
-	result = Recursive(n);
+	for (int i = 1; i < 10; i++) {
 
-	printf("%d\n", n, result);
+		result = Recursive(i);
+
+		printf("一般的な時給 %d\n", y);
+		printf("再帰的な時給 %d\n", result);
+
+		y += 1072;
+	}
+
+	
 
 	return 0;
 }
