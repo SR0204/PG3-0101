08d2f0b3dfab5b82803f2e75495d682b23a2c132
SR0204 <k023g0023@g.neec.ac.jp>
2024-11-27 19:54:28 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,12 +1,65 @@
-#include<stdio.h>
-#include<iostream>
-#include<Windows.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <list>
+using namespace std;
 
-int main() {
+int main()
+{
+	
+	list<string> station{
+		"Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno",
+		"Uguisudani", "Nippori", "Tabata", "Komagome", "Sugamo",
+		"Otsuka", "Ikebukuro", "Mejiro", "Takadanobaba", "Shin-Okubo",
+		"Shinjyuku", "Yoyogi", "Harajyuku", "Shibuya", "Ebisu",
+		"Meguro", "Gotanda", "Osaki", "Shinagawa", "Tamachi",
+		"Hamamatsucho", "Shimbashi", "Yurakucho"
+	};
 
-	//SetConsoleOutputCP(65001);
+	printf("1970年\n");
+	//1970年の駅を表示
+	for (auto itr = station.begin(); itr != station.end(); ++itr)
+	{
+		std::cout << *itr << "\n";
+	}
 
-	//char str[] = "ソ";
-	printf("東京ゲームショウ");
+	printf("\n2019年\n");
+
+	//要素の追加
+	for (std::list<string>::iterator itr = station.begin(); itr != station.end(); ++itr)
+	{
+		if (*itr == "Tabata")
+		{
+			itr = station.insert(itr, "Nishi-Nippori");
+			++itr;
+		}
+	}
+
+	//2019年の駅を表示
+	for (auto itr = station.begin(); itr != station.end(); ++itr)
+	{
+		std::cout << *itr << "\n";
+	}
+
+
+	printf("\n2022年\n");
+
+	//要素の追加
+	for (std::list<string>::iterator itr = station.begin(); itr != station.end(); ++itr)
+	{
+		if (*itr == "Tamachi")
+		{
+			itr = station.insert(itr, "Takanawa-Geteway");
+			++itr;
+		}
+	}
+
+	//2022年の駅を表示
+	for (auto itr = station.begin(); itr != station.end(); ++itr)
+	{
+		std::cout << *itr << "\n";
+	}
+
 	return 0;
 }
