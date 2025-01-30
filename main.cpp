d2714630ff966c80a6ca1c634a828b24450a7c3d
SR0204 <k023g0023@g.neec.ac.jp>
2025-01-30 15:43:53 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -1,20 +1,14 @@
 #include <iostream>
 #include <thread>
 
-using namespace std;
-
-void PrintThread(uint32_t num) {
-	cout << "thread No" << num << endl;
-}
-
 int main() {
 
-	thread t1(PrintThread, 1);
-	t1.join();
-	thread t2(PrintThread, 2);
-	t2.join();
-	thread t3(PrintThread, 3);
-	t3.join();
+	int&& c = 1;
+
+	std::move(c);
+
+	//ムーブコンストラクタ
+
 
 	return 0;
 }
