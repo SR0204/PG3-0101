1b367ee66deddc68db3a807b8ea8e1a416c36867
SR0204 <k023g0023@g.neec.ac.jp>
2025-02-03 14:21:02 +0900

    多分完了

--- main.cpp
+++ main.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 #include <thread>
+#include <chrono>
+#include <stdio.h>
+#include <string>
 
 int main() {
 
-	int&& c = 1;
+	
+	std::string a(1000000, 'a');
+	std::string aCopy;
+	std::string aMove;
 
-	std::move(c);
+	// 開始時刻を取得
+	auto startCopy = std::chrono::high_resolution_clock::now();
 
-	//ムーブコンストラクタ
 
+	aCopy = a;
+
+
+	// 終了時刻を取得
+	auto endCopy = std::chrono::high_resolution_clock::now();
+
+	// 経過時間をマイクロ秒単位で計算
+	auto durationCopy = std::chrono::duration_cast<std::chrono::microseconds>(endCopy - startCopy);
+
+	// 結果を表示
+	std::cout << "コピー時間: " << durationCopy.count() << " μs\n";
 
 	return 0;
 }
